19d6be431adcae357ccfacf7f273f543b8c1e83f
Chuk <chukm98@gmail.com>
2016-12-17 23:01:59 +0300

    Добавил элементарную навигацию по магазину

--- Store/Store/Store/Pages/Listing.aspx.cs
+++ Store/Store/Store/Pages/Listing.aspx.cs
@@ -12,10 +12,23 @@ namespace Store.Pages
     public partial class Listing : System.Web.UI.Page
     {
         private Repository repository = new Repository();
+        private int pageSize = 4;
+
+        protected int CurrentPage
+        {
+            get
+            {
+                int page;
+                return int.TryParse(Request.QueryString["page"], out page) ? page : 1;
+            }
+        }
 
         protected IEnumerable<Gadget> GetGadgets()
         {
-            return repository.Gadgets;
+            return repository.Gadgets
+                .OrderBy(g => g.GadgetId)
+                .Skip((CurrentPage - 1) * pageSize)
+                .Take(pageSize);
         }
 
         protected void Page_Load(object sender, EventArgs e)
