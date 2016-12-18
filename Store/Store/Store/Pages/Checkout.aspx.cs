ab6c2dd380237f21a88e54da666c16d126c689b9
MikhailChuk <chukm98@gmail.com>
2016-12-18 10:17:06 +0300

    Revert "Revert "Форма осуществления заказа""
    This reverts commit 2c3eceb8e318f2172ea3cca5ce135a3eff1f67f1.

---
+++ Store/Store/Store/Pages/Checkout.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Store.Models;
+using Store.Models.Repository;
+using Store.Pages.Helpers;
+using System.Web.ModelBinding;
+
+namespace Store.Pages
+{
+    public partial class Checkout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            checkoutForm.Visible = true;
+            checkoutMessage.Visible = false;
+
+            if (IsPostBack)
+            {
+                Order myOrder = new Order();
+                if (TryUpdateModel(myOrder,
+                   new FormValueProvider(ModelBindingExecutionContext)))
+                {
+
+                    myOrder.OrderLines = new List<OrderLine>();
+
+                    Cart myCart = SessionHelper.GetCart(Session);
+
+                    foreach (CartLine line in myCart.Lines)
+                    {
+                        myOrder.OrderLines.Add(new OrderLine
+                        {
+                            Order = myOrder,
+                            Gadget = line.Gadget,
+                            Quantity = line.Quantity
+                        });
+                    }
+
+                    new Repository().SaveOrder(myOrder);
+                    myCart.Clear();
+
+                    checkoutForm.Visible = false;
+                    checkoutMessage.Visible = true;
+                }
+            }
+        }
+    }
+}
